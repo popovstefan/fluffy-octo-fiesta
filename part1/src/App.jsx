4521bc51d4b0927a160bdf3c12a348ec21723c79
Stefan Popov <steffan.popov@protonmail.com>
2024-01-29 20:36:20 +0100

    part1, exercises 1.1 & 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,25 +1,9 @@
-const Hello = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old!
-      </p>
-    </div>
-  )
-}
-
-
 const App = () => {
-  const name = "Peter"
-  const age = 10
+  const friends = [ 'Peter', 'Maya']
+
   return (
     <div>
-      <h1>Greetings!</h1>
-      <Hello name="Maya" age={26 + 10}/>
-      <Hello name={name} age={age}/>
+      <p>{friends}</p>
     </div>
   )
 }
-
-export default App
